8a32b2804869fb7fd65f2df6310a0b6621ced697
Steven Samir <106476257+stevensamir20@users.noreply.github.com>
2022-07-13 21:28:54 +0200

    Add files via upload

---
+++ module4-solution/script.js
@@ -0,0 +1,20 @@
+var names = new Array();
+names[0] = "Yaakov";
+names[1] = "John";
+names[2] = "Jen";
+names[3] = "Jason";
+names[4] = "Paul";
+names[5] = "Frank";
+names[6] = "Larry";
+names[7] = "Paula";
+names[8] = "Laura";
+names[9] = "Jim";
+
+for (var i = 0; i < names.length; i++) {
+   if (names[i].charAt()=='J' || names[i].charAt()==='j') {
+        console.log("Goodebye " + names[i]);
+   } 
+   else {
+        console.log("Hello " + names[i]);
+   }
+}
